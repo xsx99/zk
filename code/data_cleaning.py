e5df1708e8b553c0f7fb810d09256b2ed2a6c570
xsx99 <shuxin.xu@gmail.com>
2017-07-09 11:26:05 -0700

    add feature data set and subset feature set

--- code/data_cleaning.py
+++ code/data_cleaning.py
@@ -7,5 +7,15 @@ Created on Fri Jul  7 14:16:00 2017
 
 import pandas as pd
 
+
+# load training set
 train = pd.read_csv('../data/train_2016_v2.csv')
-print(train.columns)
+train['transactiondate'] = pd.to_datetime(train['transactiondate'])
+
+
+# subset feature set
+parcelid = list(train['parcelid'])
+feature = pd.read_csv('../data/properties_2016.csv')
+feature = feature.ix[feature['parcelid'].isin(parcelid)]
+
+# 
