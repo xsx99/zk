03dcde527178da7cffac85aed0cb2c906ce19684
xsx99 <shuxin.xu@gmail.com>
2017-07-09 17:29:35 -0700

    add missing value check and split data functions

--- code/data_cleaning.py
+++ code/data_cleaning.py
@@ -6,6 +6,11 @@ Created on Fri Jul  7 14:16:00 2017
 """
 
 import pandas as pd
+import numpy as np
+import datetime as dt
+import library as lb
+import imp
+imp.reload(lb)
 
 
 # load training set
@@ -13,10 +18,47 @@ train = pd.read_csv('../data/train_2016_v2.csv')
 train['transactiondate'] = pd.to_datetime(train['transactiondate'])
 
 
+
 # subset feature set
 parcelid = list(train['parcelid'])
-feature = pd.read_csv('../data/properties_2016.csv')
+feature = pd.read_csv('../data/properties_2016_subset.csv')
 feature = feature.ix[feature['parcelid'].isin(parcelid)]
-feature.to_csv('../data/properties_2016_subset.csv', index=False)
+#feature.to_csv('../data/properties_2016_subset.csv', index=False)
 
-# 
+
+
+
+# explore time series model
+logerror = train.groupby(['transactiondate'])['logerror'].mean()   
+lb.pacf_plot(logerror)
+
+
+
+# add month into feature set
+train['month'] = train['transactiondate'].apply(lambda x: x.month)
+train = pd.merge(feature, train, on='parcelid', how='right')
+
+
+
+# check missing value
+na=lb.na_check(train)
+
+# parce catgorical varibles
+
+# scale variables
+
+# sperate training set to be two parts, for training and validation each
+train_x,train_y,test_x,test_y = lb.data_partition(train,min(train['transactiondate']),\
+min(train['transactiondate'])+dt.timedelta(days=90),\
+min(train['transactiondate'])+dt.timedelta(days=90),'transactiondate','logerror')
+
+
+
+
+
+
+
+
+ 
+
+
---
+++ code/library.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  9 17:08:40 2017
+
+@author: Shuxin Xu
+"""
+
+
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+import matplotlib.pyplot as plt
+
+import statsmodels.tsa.api as tsa
+
+
+
+def pacf_plot(var,I=0):
+    
+    ''' Plot partial autocorrelation function of the input time series
+        var: pandas series
+            input time series
+        I: integer
+            number of time differences to be performed on the time series
+    '''
+    
+    while (I!=0):
+        var=var-var.shift(1)
+        I=I-1
+    var=var.ix[~pd.isnull(var)]
+    tsa.graphics.plot_pacf(var,lags=90)
+    plt.title(var.name+' partial autocorrelation')
+
+
+
+
+def na_check(df):
+    na = []
+    cols = df.columns
+    n = df.shape[0]
+    
+    for col in cols:
+        na.append(1 - np.sum(pd.isnull(df[col]))/n)    
+    na=pd.DataFrame({'value':na, 'name':cols})
+    na.sort_values(by='value', ascending=True, inplace=True)
+    na.reset_index(inplace=True, drop=True)
+
+    fig = plt.figure(figsize=(10,15))
+    plt.barh(na.index, na['value'], align='center')
+    plt.yticks(na.index, na['name'])
+    
+    plt.xlabel('Missing Value%')
+    plt.title('Feature Missing Value Analysis')
+    plt.tight_layout()
+    plt.savefig('missing value check.jpg')
+    
+    return na
+    
+    
+    
+def data_partition(df,st,ed,spt,date_col,predition_col):
+    
+    ''' Partition data into training set and testing set
+        
+        Parameters
+        ----------
+        df: pandas dataframe
+            data to be partitioned
+        st: string
+            start timestamp of the training set
+        ed: string
+            end timestamp of the testing set
+        spt: string
+            timestamp splitting training and testing set
+        
+        Returns
+        -------
+        training: pandas dataframe
+        testing: pandas dataframe
+    '''
+    
+    training = df.ix[(df[date_col]>st)&(df[date_col]<=spt),:]
+    testing = df.ix[(df[date_col]>spt)&(df[date_col]<=ed)]
+    train_y = training[predition_col]
+    train_x = training.ix[:,training.columns != predition_col]
+    test_y = testing[predition_col]
+    test_x = testing.ix[:,testing.columns != predition_col]
+
+    return train_x,train_y,test_x,test_y
